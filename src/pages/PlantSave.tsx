06a970a5dac4e9affa704a820ae88b201c374b44
Emerson <emersonlucenaoficial@gmail.com>
2021-04-26 21:54:49 -0300

    added time editor of notification

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
   StyleSheet,
@@ -10,6 +10,9 @@ import {
   TouchableOpacity
 } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { useRoute } from '@react-navigation/core';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
+
 
 import { SvgFromUri } from 'react-native-svg';
 import { Button } from '../components/Button';
@@ -17,24 +20,69 @@ import colors from '../styles/colors';
 
 import waterdrop from '../assets/waterdrop.png'
 import fonts from '../styles/fonts';
+import { format, isBefore } from 'date-fns';
+
+
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number,
+      repeat_every: string
+    }
+  }
+}
+
 
 export function PlantSave() {
+
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS === 'ios');
+
+  const route = useRoute();
+
+  const { plant } = route.params as Params;
+
+  function handleChangeTime(event: Event, dateTime: Date | undefined) {
+    if (Platform.OS === 'android') {
+      setShowDatePicker(oldstate => !oldstate);
+    }
+
+    if (dateTime && isBefore(dateTime, new Date())) {
+      setSelectedDateTime(new Date());
+      return Alert.alert('OOPS...', 'Escolha um horário válido! ⏱')
+    }
+
+    if (dateTime) {
+      setSelectedDateTime(dateTime);
+    }
+
+  }
+
+  function handleOpenDateTimePickerForAndroid() {
+    setShowDatePicker(oldstate => !oldstate);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri
-          uri=""
+          uri={plant.photo}
           height={150}
           width={150}
         />
 
         <Text style={styles.plantName}>
-          Nome da planta
-      </Text>
+          {plant.name}
+        </Text>
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Suscipit dolorem tenetur repellendus cupiditate sit vero?
-      </Text>
+          {plant.about}
+        </Text>
       </View>
       <View style={styles.controller}>
         <View style={styles.tipContainer}>
@@ -43,19 +91,36 @@ export function PlantSave() {
             style={styles.tipImage}
           />
           <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing.
-        </Text>
+            {plant.water_tips}
+          </Text>
         </View>
+        <Text style={styles.alertLabel}>
+          Escolha o melhor horário para ser lembrado:
+        </Text>
+
+        {showDatePicker && <DateTimePicker
+          value={selectedDateTime}
+          mode="time"
+          display="default"
+          onChange={handleChangeTime}
+        />}
+        {
+          Platform.OS === 'android' && (
+            <TouchableOpacity
+              style={styles.dateTimePickerButton}
+              onPress={handleOpenDateTimePickerForAndroid}
+            >
+              <Text style={styles.dateTimePickerText}>
+                {`Mudar ${format(selectedDateTime, 'HH:mm')}.`}
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+        <Button
+          title="Cadastrar planta"
+          onPress={() => { }}
+        />
       </View>
-
-      <Text style={styles.alertLabel}>
-        Escolha o melhor horário para ser lembrado:
-      </Text>
-
-      <Button
-        title="Cadastrar planta"
-        onPress={() => { }}
-      />
     </View>
   )
 }
@@ -65,7 +130,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: colors.shape,
-
   },
   plantInfo: {
     flex: 1,
@@ -102,7 +166,6 @@ const styles = StyleSheet.create({
     position: 'relative',
     bottom: 60,
     borderRadius: 20,
-
   },
   tipImage: {
     width: 56,
@@ -122,6 +185,15 @@ const styles = StyleSheet.create({
     color: colors.heading,
     fontSize: 12,
     marginBottom: 5,
+  },
+  dateTimePickerText: {
+    color: colors.heading,
+    fontSize: 24,
+    fontFamily: fonts.text,
+  },
+  dateTimePickerButton: {
+    width: '100%',
+    alignItems: 'center',
+    paddingVertical: 40,
   }
-
 })
