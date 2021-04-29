578f29e6bb6b9e4c82d6234983f0e443d1b85b19
Emerson <emersonlucenaoficial@gmail.com>
2021-04-28 22:12:30 -0300

    app finished

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -35,6 +35,8 @@ export async function savePlant(plant: PlantProps): Promise<void> {
     if (repeat_every === 'week') {
       const interval = Math.trunc(7 / times);
       nextTime.setDate(now.getDate() + interval);
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1);
     }
 
     const seconds = Math.abs(
